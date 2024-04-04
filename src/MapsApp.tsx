8fc194a4dba751b691c47439f9aa60eb048eb80c
Carlos Carvajal <cxcarvaj@gmail.com>
2024-04-03 22:34:37 -0500

    Configuration of the app in order to use icons

---
+++ src/MapsApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const MapsApp = () => {
+  return (
+    <View>
+      <Text>MapsApp</Text>
+      <Icon name="home" size={30} color="black" />
+    </View>
+  );
+};
