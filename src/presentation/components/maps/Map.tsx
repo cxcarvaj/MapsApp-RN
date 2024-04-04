5faa23fd68a9e66355f61b2da0d035aff85fd83d
Carlos Carvajal <cxcarvaj@gmail.com>
2024-04-04 09:28:37 -0500

    Improving the MapsScreen

--- src/presentation/components/maps/Map.tsx
+++ src/presentation/components/maps/Map.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import {Platform} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {Location} from '../../../infrastructure/interfaces/location';
 
-export const Map = () => {
+interface Props {
+  showsUserLocation?: boolean;
+  initialLocation: Location;
+}
+
+export const Map = ({showsUserLocation = true, initialLocation}: Props) => {
   return (
     <>
       <MapView
+        showsUserLocation={showsUserLocation}
         provider={Platform.OS === 'android' ? PROVIDER_GOOGLE : undefined} // remove if not using Google Maps
         style={{flex: 1}}
         region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: initialLocation.latitude,
+          longitude: initialLocation.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}>
--- src/presentation/screen/maps/MapsScreen.tsx
+++ src/presentation/screen/maps/MapsScreen.tsx
@@ -3,11 +3,7 @@ import {StyleSheet, View} from 'react-native';
 import {Map} from '../../components/maps/Map';
 
 export const MapsScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Map />
-    </View>
-  );
+  return <View style={styles.container}>{/* <Map /> */}</View>;
 };
 
 const styles = StyleSheet.create({
