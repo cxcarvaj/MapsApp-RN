1e45be497ec981f25001b6b1511a060bfd2b1965
Carlos Carvajal <cxcarvaj@gmail.com>
2024-04-04 10:49:28 -0500

    Final commit of the project

--- src/presentation/components/maps/Map.tsx
+++ src/presentation/components/maps/Map.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {Platform} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Polyline} from 'react-native-maps';
 import {Location} from '../../../infrastructure/interfaces/location';
 import {FAB} from '../ui/FAB';
 import {useLocationStore} from '../../store/location/useLocationStore';
@@ -16,9 +16,15 @@ export const Map = ({showsUserLocation = true, initialLocation}: Props) => {
   const cameraLocation = useRef<Location>(initialLocation);
 
   const [isFollowingUser, setIsFollowingUser] = useState(true);
+  const [isShowingPolyline, setIsShowingPolyline] = useState(true);
 
-  const {getLocation, lastKnownLocation, watchLocation, clearWatchLocation} =
-    useLocationStore();
+  const {
+    getLocation,
+    lastKnownLocation,
+    watchLocation,
+    clearWatchLocation,
+    userLocationHistory,
+  } = useLocationStore();
 
   const moveCameraToLocation = (location: Location) => {
     if (!mapRef.current) {
@@ -76,7 +82,24 @@ export const Map = ({showsUserLocation = true, initialLocation}: Props) => {
           title="My Marker"
           description="Some description"
         /> */}
+
+        {isShowingPolyline && (
+          <Polyline
+            coordinates={userLocationHistory}
+            strokeColor="black"
+            strokeWidth={5}
+          />
+        )}
       </MapView>
+
+      <FAB
+        iconName={isShowingPolyline ? 'eye-outline' : 'eye-off-outline'}
+        onPress={() => setIsShowingPolyline(!isShowingPolyline)}
+        style={{
+          bottom: 140,
+          right: 20,
+        }}
+      />
 
       <FAB
         iconName={isFollowingUser ? 'walk-outline' : 'accessibility-outline'}
